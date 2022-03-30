e86cfef6236b7ebc3b02b3332c5425ca456c1e0d
Esperlluette <weezy.chang@gmail.com>
2022-03-30 17:17:02 +0200

    commit

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-console.log(`${process.env.CONS_SECRET}`)
-console.log(`Ready on ${process.env.CONS_KEY} `)
-console.log(`API : ${process.env.ACCESS_TOKEN}`);
+const TwitterLite = require('twitter-lite')
+
+console.log("+---------------------------+"+ 
+          "\n|                           |"+ 
+          "\n|      Début du script      |"+
+          "\n|                           |"+
+          "\n+---------------------------+")
+;
+
+const app = new TwitterLite({
+    version: "2",
+    extension: false, 
+    bearer_token: process.env.BEARER_TOKEN
+  })
+
+  const user = new TwitterLite({  
+    access_token_key: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_SECRET,
+    consumer_key: process.env.CONS_KEY,
+    consumer_secret: process.env.CONS_SECRET
+  })
